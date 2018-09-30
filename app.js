a92fe93d34856ce697cfa68abc11410787956cd4
Phil Stainer <philstainer.dev@gmail.com>
2018-09-30 17:16:39 +0100

    Add dotenv and .env setup

--- app.js
+++ app.js
@@ -11,12 +11,18 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+  app.use(logger('dev'));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(process.env.PORT, () => console.log(`Starting app on port ${process.env.PORT}`));
 
 module.exports = app;
