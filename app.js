c167dfc65c975cb45169c71c09c8a02737f9d474
Phil Stainer <philstainer.dev@gmail.com>
2018-09-30 19:17:07 +0100

    Add auth route and login page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter)
 
 app.listen(process.env.PORT, () => console.log(`Starting app on port ${process.env.PORT}`));
 
