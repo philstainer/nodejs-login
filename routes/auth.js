0d447508f1517d972ada398ebd4fa871d83235e9
Phil Stainer <philstainer.dev@gmail.com>
2018-10-01 20:24:16 +0100

    Add signup route and page

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/login', function (req, res, next) {
+/* GET login page. */
+router.get('/login', (req, res, next) => {
   res.render('login.ejs');
 });
+
+/* GET signup page. */
+router.get('/signup', (req, res, next) => {
+  res.render('signup.ejs');
+});
+
+router.post('/signup', (req, res, next) => {
+  console.log(req.body)
+})
 
 module.exports = router;
